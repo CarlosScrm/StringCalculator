cb905960a05c8f121520fa3e76ff5bb46752cf05
Carlos Ramirez <carlos.ramirez@scrm.eu>
2022-02-17 23:11:29 +0100

    Initial Commit

---
+++ StringCalculatorTest/StringCalculator_Add.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace StringCalculatorTest
+{
+    public class StringCalculator_Add
+    {
+        [Fact]
+        public void Given_Empty_string_then_Returns_0()
+        {
+
+        }
+    }
+}
