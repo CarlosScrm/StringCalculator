dff52cfd5d28360b3912e53376689d775d52e152
Carlos Ramirez <carlos.ramirez@scrm.eu>
2022-02-17 23:32:50 +0100

    Finish 3 numbers test

--- StringCalculatorTest/StringCalculator_Add.cs
+++ StringCalculatorTest/StringCalculator_Add.cs
@@ -4,12 +4,12 @@ namespace StringCalculatorTest
 {
     public class StringCalculator_Add
     {
+        private StringCalculator _calculator = new StringCalculator();
+
         [Fact]
         public void Given_Empty_string_then_Returns_0()
         {
-            var calculator = new StringCalculator();
-
-            var result = calculator.Add("");
+            var result = _calculator.Add("");
 
             Assert.Equal(0, result);
         }
@@ -19,9 +19,7 @@ namespace StringCalculatorTest
         [InlineData("2", 2)]
         public void Given_string_number_then_Returns_number(string numbers, int expectedResult)
         {
-            var calculator = new StringCalculator();
-
-            var result = calculator.Add(numbers);
+            var result = _calculator.Add(numbers);
 
             Assert.Equal(expectedResult, result);
         }
@@ -29,11 +27,19 @@ namespace StringCalculatorTest
         [Theory]
         [InlineData("1,2", 3)]
         [InlineData("2,3", 5)]
-        public void Given_comma_separated_numbers_then_Returns_sum(string numbers, int expectedResult)
+        public void Given_Two_comma_separated_numbers_then_Returns_sum(string numbers, int expectedResult)
         {
-            var calculator = new StringCalculator();
+            var result = _calculator.Add(numbers);
 
-            var result = calculator.Add(numbers);
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("2,3,4", 9)]
+        public void Given_Three_comma_separated_numbers_then_Returns_sum(string numbers, int expectedResult)
+        {
+            var result = _calculator.Add(numbers);
 
             Assert.Equal(expectedResult, result);
         }
